959c034dd5d279c65c0c3a1e1d374c3dbe0ac546
njshakib <nashakib79@gmail.com>
2026-09-05 21:48:41 +0600

    Add files via upload

---
+++ sql/07_coalfieldsupplies_data.sql
@@ -0,0 +1,10 @@
+-- =========================================================
+-- CoalFieldSupplies table -- FULL DATA (1 rows)
+-- 1 field-to-plant supply link (Barapukuria field to Barapukuria plant) -- depends on CoalField, CoalPowerPlant
+-- =========================================================
+
+USE energy_data;
+
+INSERT INTO CoalFieldSupplies (field_id, plant_id) VALUES
+(1, 1);
+
