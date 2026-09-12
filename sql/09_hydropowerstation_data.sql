100119f6a8cb759f23269b7d0bd4fc63921e523a
njshakib <nashakib79@gmail.com>
2026-09-12 21:29:33 +0600

    Add files via upload

---
+++ sql/09_hydropowerstation_data.sql
@@ -0,0 +1,10 @@
+-- =========================================================
+-- HydropowerStation table -- FULL DATA (1 rows)
+-- 1 station (Kaptai) -- depends on Organization
+-- =========================================================
+
+USE energy_data;
+
+INSERT INTO HydropowerStation (station_name, project_location, main_river, installed_capacity_mw, org_id) VALUES
+('Kaptai Hydropower Station', 'Kaptai, Rangamati', 'Karnaphuli', 230, 16);
+
