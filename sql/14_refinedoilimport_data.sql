ddc347dbca532ac455e9d6e4ddf8d6882b69b8e7
njshakib <nashakib79@gmail.com>
2026-09-12 21:59:40 +0600

    Add files via upload

---
+++ sql/14_refinedoilimport_data.sql
@@ -0,0 +1,36 @@
+-- =========================================================
+-- RefinedOilImport table -- FULL DATA (27 rows)
+-- 27 rows = 9 fiscal years x 3 product categories, refined_oil_import.xlsx, BPC
+-- =========================================================
+
+USE energy_data;
+
+INSERT INTO RefinedOilImport (product_name, fiscal_year, value, quantity) VALUES
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2016-17', 14433.91, 3871432),
+('Lubricating Base Oil', '2016-17', 0, 0),
+('Furnace Oil', '2016-17', 1240.66, 521199),
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2017-18', 23300.67, 4892089),
+('Lubricating Base Oil', '2017-18', 0, 0),
+('Furnace Oil', '2017-18', 2091.52, 650540),
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2018-19', 23376.5, 4281958),
+('Lubricating Base Oil', '2018-19', 0, 0),
+('Furnace Oil', '2018-19', 1282.49, 318634),
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2019-20', 17045.81, 3873131.61),
+('Lubricating Base Oil', '2019-20', 0, 0),
+('Furnace Oil', '2019-20', 687.04, 175693.95),
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2020-21', 16805.64, 4174726.06),
+('Lubricating Base Oil', '2020-21', 0, 0),
+('Furnace Oil', '2020-21', 151.41, 47923.72),
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2021-22', 38276.98, 4809132),
+('Lubricating Base Oil', '2021-22', 0, 0),
+('Furnace Oil', '2021-22', 1710.86, 316086.19),
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2022-23', 48795.87, 4850851),
+('Lubricating Base Oil', '2022-23', 0, 0),
+('Furnace Oil', '2022-23', 2146.07, 435592),
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2023-24', 42067.74, 4388063),
+('Lubricating Base Oil', '2023-24', 0, 0),
+('Furnace Oil', '2023-24', 4102.61, 660856.76),
+('Jet-A-1/SKO/Mogas/HSD & Marine Fuel', '2024-25', 35632.52, 4091845.49),
+('Lubricating Base Oil', '2024-25', 0, 0),
+('Furnace Oil', '2024-25', 3145.94, 516036.47);
+
